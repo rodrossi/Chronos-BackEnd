bad0eaaf074d1cd0009ae47ae47994c5ee3a4c18
Hugo Souza <hsaraujo98@outlook.com>
2020-01-31 16:08:52 -0300

    Post completo, cadastrando usuario e salvando no cluster do Mongo na nuvem

--- br.com.chronos/src/main/java/br/com/chronos/services/UserService.java
+++ br.com.chronos/src/main/java/br/com/chronos/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.chronos.domain.User;
+import br.com.chronos.dtos.commands.UserInsertCommand;
 import br.com.chronos.repositories.UserRepository;
 
 @Service
@@ -15,8 +16,14 @@ public class UserService {
 	private UserRepository userRepository;
 
 	public List<User> list() {
-
+		
 		return userRepository.findAll();
+	}
+
+	public User insert(UserInsertCommand command) {
+		
+		User user = new User(command);
+		return userRepository.save(user);
 	}
 
 }
