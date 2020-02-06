6aecaf0bd0d61aac860d1791b7615e60be3ab30b
Samuel Macedo <samuel.santos@cubotecnologia.com.br>
2020-02-06 12:10:56 -0300

    Refatoração das funções de Update

--- br.com.chronos/src/main/java/br/com/chronos/domain/User.java
+++ br.com.chronos/src/main/java/br/com/chronos/domain/User.java
@@ -5,55 +5,52 @@ import org.springframework.data.annotation.Id;
 import br.com.chronos.dtos.commands.UserUpdateCommand;
 
 public class User {
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
 	private String phone;
 	private boolean active;
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public String getPhone() {
 		return phone;
 	}
-	
+
 	public boolean isActive() {
 		return active;
 	}
-	
+
 	protected User() {
-		
+
 	}
 
 	public User(String name, String email, String phone, boolean active) {
-		
-		//command.validate();
-		
+
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.active = active;
 	}
-	
+
 	public void update(UserUpdateCommand command) {
+		
 		this.name = command.getName();
 		this.email = command.getEmail();
 		this.phone = command.getPhone();
 		this.active = command.getActive();
-		
 	}
 
 }
-
